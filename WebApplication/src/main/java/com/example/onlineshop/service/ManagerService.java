276056c7361c4414d5a80fc5b72c99d42f720ad5
IoriKimura <sergeykonstwork@gmail.com>
2023-05-31 20:07:43 +0300

    Update ManagerService.java

--- WebApplication/src/main/java/com/example/onlineshop/service/ManagerService.java
+++ WebApplication/src/main/java/com/example/onlineshop/service/ManagerService.java
@@ -1,5 +1,6 @@
 package com.example.onlineshop.service;
 
+import com.example.onlineshop.dataBases.dao.CategoryDao;
 import com.example.onlineshop.dataBases.dao.ProductDao;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
@@ -10,7 +11,13 @@ import org.springframework.stereotype.Service;
 public class ManagerService {
 
     private final ProductDao productDao;
+
+    private final CategoryDao categoryDao;
     public String findAllProducts() {
         return new Gson().toJson(productDao.findAll());
+    }
+
+    public String findAllCategories() {
+        return new Gson().toJson(categoryDao.findAll());
     }
 }
